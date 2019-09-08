3a2b25db5aa5c50f7e4e9305f142134d2ac6f254
Juanpa <sobraljuanpa@gmail.com>
2019-09-08 18:03:11 -0300

    Created domain and test packages

---
+++ TwoDrive.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TwoDrive.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ TwoDrive.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TwoDrive.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
