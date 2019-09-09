07c229f417368f884f0dbdcfa80a0c8ef7be391c
Jose Pablo Goñi <josepablogoni@gmail.com>
2019-09-09 10:44:20 -0300

    CreateFolderClass

---
+++ TwoDrive.Test/DomainTest/FolderTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TwoDrive.Test
+{
+    [TestClass]
+    public class FolderTest
+    {
+        FolderTest folder;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            folder = new Folder();
+            
+        }
+        [TestMethod]
+        public void SetIDTest()
+        {
+            Guid aux = new Guid("AC937AJS8-6EBE-4A07-BEB9-B7AC59485B90");
+            folder.Id = aux;
+            Assert.AreEqual(aux, folder.Id);
+        }
+    }
+}
