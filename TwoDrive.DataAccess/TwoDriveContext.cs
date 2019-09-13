a65d4166206bb233c68f9fd38e7108665c68bdd7
Juanpa <sobraljuanpa@gmail.com>
2019-09-12 23:24:57 -0300

    Created TwoDriveContext class

---
+++ TwoDrive.DataAccess/TwoDriveContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TwoDrive.Domain;
+
+namespace TwoDrive.DataAccess
+{
+    public class TwoDriveContext : DbContext
+    {
+        public TwoDriveContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
