79e3f8db10543e7b12341726f0a2af4785ce8eb0
Juanpa <sobraljuanpa@gmail.com>
2019-09-14 20:28:39 -0300

    Finished FolderRepository class

---
+++ TwoDrive.DataAccess/FolderRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using TwoDrive.Domain;
+
+namespace TwoDrive.DataAccess
+{
+    public class FolderRepository : IDataRepository<Folder>
+    {
+        readonly TwoDriveContext _context;
+
+        public FolderRepository(TwoDriveContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Folder> GetAll()
+        {
+            return _context.Folders.ToList();
+        }
+
+        public Folder Get(long id)
+        {
+            return _context.Folders.FirstOrDefault(
+                f => f.Id == id);
+        }
+
+        public void Add(Folder folder)
+        {
+            _context.Folders.Add(folder);
+        }
+
+        public void Update(Folder oldFolder, Folder newFolder)
+        {
+            newFolder.Files = oldFolder.Files;
+            newFolder.Folders = oldFolder.Folders;
+            newFolder.Name = oldFolder.Name;
+            newFolder.OwnerId = oldFolder.OwnerId;
+            newFolder.Parent = oldFolder.Parent;
+            newFolder.Readers = oldFolder.Readers;
+
+            _context.SaveChanges();
+        }
+
+        public void Delete(Folder folder)
+        {
+            _context.Folders.Remove(folder);
+            _context.SaveChanges();
+        }
+    }
+}
