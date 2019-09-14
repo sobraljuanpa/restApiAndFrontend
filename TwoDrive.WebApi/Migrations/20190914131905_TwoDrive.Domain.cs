9b4974ca82e4898588d1e6ff4d9e7e4868870f71
Juanpa <sobraljuanpa@gmail.com>
2019-09-14 10:25:11 -0300

    Added new DbSets to context, created and applied migrations

---
+++ TwoDrive.WebApi/Migrations/20190914131905_TwoDrive.Domain.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwoDrive.WebApi.Migrations
+{
+    public partial class TwoDriveDomain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_users_UserId",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "Users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_users_UserId",
+                table: "Users",
+                newName: "IX_Users_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Users_UserId",
+                table: "Users",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Users_UserId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_UserId",
+                table: "users",
+                newName: "IX_users_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_users_UserId",
+                table: "users",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ TwoDrive.WebApi/Migrations/20190914132305_TwoDrive.UpdatedContext.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwoDrive.WebApi.Migrations
+{
+    public partial class TwoDriveUpdatedContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FileId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "FolderId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "FolderElement",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ParentId = table.Column<long>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(nullable: true),
+                    OwnerId = table.Column<long>(nullable: true),
+                    FolderId = table.Column<long>(nullable: true),
+                    Folder_OwnerId = table.Column<long>(nullable: true),
+                    FolderId1 = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FolderElement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FolderElement_FolderElement_FolderId",
+                        column: x => x.FolderId,
+                        principalTable: "FolderElement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FolderElement_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FolderElement_FolderElement_FolderId1",
+                        column: x => x.FolderId1,
+                        principalTable: "FolderElement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FolderElement_Users_Folder_OwnerId",
+                        column: x => x.Folder_OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FolderElement_FolderElement_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "FolderElement",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_FileId",
+                table: "Users",
+                column: "FileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_FolderId",
+                table: "Users",
+                column: "FolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FolderElement_FolderId",
+                table: "FolderElement",
+                column: "FolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FolderElement_OwnerId",
+                table: "FolderElement",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FolderElement_FolderId1",
+                table: "FolderElement",
+                column: "FolderId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FolderElement_Folder_OwnerId",
+                table: "FolderElement",
+                column: "Folder_OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FolderElement_ParentId",
+                table: "FolderElement",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_FolderElement_FileId",
+                table: "Users",
+                column: "FileId",
+                principalTable: "FolderElement",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_FolderElement_FolderId",
+                table: "Users",
+                column: "FolderId",
+                principalTable: "FolderElement",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_FolderElement_FileId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_FolderElement_FolderId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "FolderElement");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_FileId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_FolderId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FolderId",
+                table: "Users");
+        }
+    }
+}
