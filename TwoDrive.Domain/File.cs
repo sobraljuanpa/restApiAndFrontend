134eb8311a2541822f8c41d84121534a997ade75
Juanpa <sobraljuanpa@gmail.com>
2019-09-14 10:13:03 -0300

    Added File class

---
+++ TwoDrive.Domain/File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoDrive.Domain
+{
+    public class File : FolderElement
+    {
+
+        public string Content { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }
+
+        public User Owner { get; set; }
+
+        public List<User> Readers { get; set; }
+
+        public void AddReader(User user)
+        {
+            Readers.Add(user);
+        }
+
+        public void RemoveReader(User user)
+        {
+            Readers.Remove(user);
+        }
+    }
+}
