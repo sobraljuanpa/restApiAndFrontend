9b2dad913ad390c7a2bcadab29a0a01428340f39
Juanpa <sobraljuanpa@gmail.com>
2019-09-14 20:06:44 -0300

    couldnt get the method working

--- TwoDrive.DataAccess/IDataRepository.cs
+++ TwoDrive.DataAccess/IDataRepository.cs
@@ -8,11 +8,6 @@ namespace TwoDrive.DataAccess
     public interface IDataRepository<T>
     {
         IEnumerable<T> GetAll();
-
-        IEnumerable<T> Get(
-            Expression<Func<T, bool>> filter = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            string includeProperties = "");
         T Get(long id);
         void Add(T entity);
         void Update(T dbEntity, T newEntity);
