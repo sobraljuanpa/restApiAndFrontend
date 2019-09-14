ea6a20f0773c4db91fc1281f6ab65fd1b1660475
Juanpa <sobraljuanpa@gmail.com>
2019-09-14 10:06:58 -0300

    Created abstract FolderElement class

---
+++ TwoDrive.Domain/FolderElement.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TwoDrive.Domain
+{
+    public abstract class FolderElement
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public FolderElement Parent { get; set; }
+    }
+}
