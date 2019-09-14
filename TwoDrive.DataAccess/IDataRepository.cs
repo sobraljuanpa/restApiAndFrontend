655a6c89c0a946940c759047b216700569ebe125
Juanpa <sobraljuanpa@gmail.com>
2019-09-14 20:03:49 -0300

    Added get with filter methods to IDataRepository

--- TwoDrive.DataAccess/IDataRepository.cs
+++ TwoDrive.DataAccess/IDataRepository.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace TwoDrive.DataAccess
 {
     public interface IDataRepository<T>
     {
         IEnumerable<T> GetAll();
+
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "");
         T Get(long id);
         void Add(T entity);
         void Update(T dbEntity, T newEntity);
