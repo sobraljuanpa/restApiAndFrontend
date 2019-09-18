77577d6ba8443919d006cc295d2c03719cbdd100
Jose Pablo Goñi <josepablogoni@gmail.com>
2019-09-18 12:52:30 -0300

    RefactoringInProgress

---
+++ TwoDrive.BusinessLogic.Interface/FolderElementLogic.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoDrive.DataAccess.Interface;
+using TwoDrive.Domain;
+
+namespace TwoDrive.BusinessLogic.Interface
+{
+    public abstract class FolderElementLogic<T> where T : FolderElement
+    {
+        IDataRepository<T> _repository;
+        IDataRepository<T> _userRepository;
+
+        public T Get(long id)
+        {
+            FolderElementExists(id);
+            return _repository.Get(id);
+        }
+        public void Delete(T Entity)
+        {
+            FolderElementExists(Entity.Id);
+            _repository.Delete(Entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void AddReader(File Entity, User user)
+        {
+            UserExist(user.Id);
+            Entity.AddReader(user);
+        }
+
+        public void RemoveReader(File Entity, User user)
+        {
+            UserExist(user.Id);
+            Entity.RemoveReader(user);
+        }
+
+        protected void FolderElementExists(long id)
+        {
+            if (_repository.Get(id) == null)
+                throw new Exception("El elemento no existe.");
+        }
+
+        protected void HasTheSameLocation(T entity, T newEntity)
+        {
+            if (!entity.Parent.Equals(newEntity.Parent))
+                throw new Exception("Las ubicaciones no coinciden.");
+        }
+
+        protected void AlreadyInFolder(T entity, Folder folder)
+        {
+            if (entity.Parent.Equals(folder))
+                throw new Exception("El archivo ya esta en esa carpeta.");
+        }
+
+        protected void ReadersExist(List<User> readers)
+        {
+            foreach (User reader in readers)
+            {
+                if (_userRepository.Get(reader.Id) == null)
+                    throw new Exception("Uno de los lectores especificados no existe.");
+            }
+        }
+
+        protected void OwnerExists(long owners)
+        {
+            if (_userRepository.Get(owners) == null)
+                throw new Exception("Uno de los dueños especificado no existe.");
+        }
+
+        protected void NameIsNull(string name)
+        {
+            if (name == "")
+                throw new Exception("El nombre del archivo está vacio.");
+        }
+
+        protected void ReadersIsNull(List<User> readers)
+        {
+            if (readers.Count == 0)
+                throw new Exception("El archivo no tiene ningun lector.");
+        }
+
+        protected void ParentIsNull(Folder parent)
+        {
+            if (parent == null)
+                throw new Exception("No existe una carpeta padre para el archivo.");
+        }
+
+        private void UserExist(long id)
+        {
+            if (_userRepository.Get(id) == null)
+                throw new Exception("El usuario no existe.");
+        }
+    }
+}
---
+++ TwoDrive.BusinessLogic/FolderLogicN.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoDrive.BusinessLogic.Interface;
+using TwoDrive.DataAccess.Interface;
+using TwoDrive.Domain;
+
+namespace TwoDrive.BusinessLogic
+{
+    public class FolderLogicN : FolderElementLogic<Folder>
+    {
+        IDataRepository<Folder> _repository;
+        public void Add(Folder entity)
+        {
+            ValidateFormat(entity);
+            entity.LastModifiedDate = DateTime.Now;
+            entity.CreationDate = DateTime.Now;
+            Folder folderBefore = entity.Parent;
+            Folder folderAfter = entity.Parent;
+            folderAfter.AddFile(entity);
+            _repository.Add(entity);
+        }
+
+        public void Update(Folder Entity, Folder newEntity)
+        {
+            newEntity.LastModifiedDate = DateTime.Now;
+            ValidateFormat(newEntity);
+            FolderElementExists(Entity.Id);
+            HasTheSameLocation(Entity, newEntity);
+            _repository.Update(Entity, newEntity);
+        }
+        public void Move(Folder Entity, Folder folder)
+        {
+            FolderElementExists(Entity.Id);
+            FolderElementExists(folder.Id);
+            AlreadyInFolder(Entity, folder);
+            Folder folderWhereFileWas = Entity.Parent;
+            Folder folderWhereIsIt = folder;
+            File filePrevious = Entity;
+            Entity.Parent.RemoveFile(Entity);
+            _folderRepository.Update(folderWhereFileWas, Entity.Parent);
+            Entity.Parent = folder;
+            folder.AddFile(Entity);
+            _repository.Update(filePrevious, Entity);
+            _folderRepository.Update(folderWhereIsIt, folder);
+        }
+
+        private void ValidateFormat(Folder entity)
+        {
+            NameIsNull(entity.Name);
+            ParentIsNull(entity.Parent);
+            ReadersIsNull(entity.Readers);
+            ContentIsNull(entity.Content);
+            OwnerExists(entity.OwnerId);
+            ReadersExist(entity.Readers);
+        }
+    }
+}
