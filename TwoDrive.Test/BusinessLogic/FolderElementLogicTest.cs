dcfdaf19025ad0710ec0e13999f78bb4bdd24027
Jose Pablo Goñi <josepablogoni@gmail.com>
2019-09-26 01:04:23 -0300

    Initialize

---
+++ TwoDrive.Test/BusinessLogic/FolderElementLogicTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using TwoDrive.BusinessLogic;
+using TwoDrive.DataAccess.Interface;
+using TwoDrive.Domain;
+
+namespace TwoDrive.Test.BusinessLogic
+{
+    [TestClass]
+    public class FolderElementLogicTest
+    {
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            
+        }
+
+    }
+}
