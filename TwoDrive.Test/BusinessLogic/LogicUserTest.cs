005038266dfe9d66b39aa6d8cee3ffa6c909f6df
Jose Pablo Goñi <josepablogoni@gmail.com>
2019-10-08 11:27:35 -0300

    AuthenticateUserInSystem

---
+++ TwoDrive.Test/BusinessLogic/LogicUserTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using TwoDrive.BusinessLogic;
+using TwoDrive.BusinessLogic.Interface;
+using TwoDrive.DataAccess.Interface;
+using TwoDrive.Domain;
+
+namespace TwoDrive.Test.BusinessLogic
+{
+    [TestClass]
+    public class LogicUserTest
+    {
+        Mock<IDataRepository<User>> userRepository;
+        Mock<IDataRepository<Folder>> folderRepository;
+        User userNull;
+        User user;
+        ILogic<User> userLogic;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            user = new User { Email = "josepablogoni@gmail.com", FirstName = "Jose Pablo", Username = "josepablogoni", FriendList = new List<User>(), LastName = "Goni", Password = "josepablo", Role = "User" };
+            user = new User();
+            userRepository = new Mock<IDataRepository<User>>();
+            folderRepository = new Mock<IDataRepository<Folder>>();
+            userLogic = new UserLogic(userRepository.Object, folderRepository.Object);
+        }
+
+        [TestMethod]
+        public void AuthenticateUserInSystem()
+        {
+            userRepository.Setup(u => u.Authenticate(user.Username, user.Password)).Returns(user);
+            userLogic.Authenticate(user.Username,user.Password);
+            userRepository.VerifyAll();
+        }
+
+    }
+}
