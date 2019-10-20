b7d079c94e690d60bbaaa9809eedb3fce0a718ec
Juanpa <sobraljuanpa@gmail.com>
2019-10-20 16:52:19 -0300

    Added login form with two way binding params

--- twodrive-frontend/src/app/app.module.ts
+++ twodrive-frontend/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
