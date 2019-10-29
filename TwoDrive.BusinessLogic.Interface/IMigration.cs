9c673187ac78ae1fe4d4a95fdf4b69f0d4ebf9d1
Jose Pablo Goñi <josepablogoni@gmail.com>
2019-10-29 13:03:39 -0300

    FirstImplementation

---
+++ TwoDrive.BusinessLogic.Interface/IMigration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoDrive.Domain;
+
+namespace TwoDrive.BusinessLogic.Interface
+{
+    public interface IMigration
+    {
+        //Voy a buscar la clase por: todas las que implementan esta interfaz y la del nombre ..
+        User GiveMeUser();
+        //PARA EL ROOT FOLDER SOLO SE PONE EL NOMBRE -rootfolder NO NECESITA CREARLO
+        List<Folder> GiveMeFolders();
+        List<File> GiveMeFiles();
+    }
+}
---
+++ TwoDrive.BusinessLogic/MigrationController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using TwoDrive.BusinessLogic.Interface;
+using TwoDrive.Domain;
+
+namespace TwoDrive.BusinessLogic
+{
+    public class MigrationController : IMigrationController
+    {
+        IMigration _migration;
+        ILogic<User> _userLogic;
+        FolderElementLogic<Folder> _folderLogic;
+        FolderElementLogic<File> _fileLogic;
+        User Owner = null;
+        Folder FolderRoot = null;
+        Regex regex = new Regex("$-rootFolder");
+
+        public MigrationController(IMigration migration, ILogic<User> userLogic, FolderElementLogic<Folder> folderLogic, FolderElementLogic<File> fileLogic)
+        {
+            _migration = migration;
+            _userLogic = userLogic;
+            _folderLogic = folderLogic;
+            _fileLogic = fileLogic;
+        }
+        
+        public void SaveAllFiles()
+        {
+            if (Owner != null)
+            {
+                try
+                {
+                    foreach (var file in _migration.GiveMeFiles())
+                    {
+                        if (regex.IsMatch(file.Parent.Name))
+                        {
+                            file.Parent = FolderRoot;
+                        }
+                        _fileLogic.Add(file);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("The format is not correct: " + e.Message);
+                }
+            }
+        }
+
+        public void SaveAllFolders()
+        {
+            if (Owner != null)
+            {
+                try
+                {
+                    foreach (var folder in _migration.GiveMeFolders())
+                    {
+                        if (regex.IsMatch(folder.Parent.Name))
+                        {
+                            folder.Parent = FolderRoot;
+                        }
+                        _folderLogic.Add(folder);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("The format is not correct: " + e.Message);
+                }
+            }
+        }
+
+        public void SaveUser()
+        {
+            try
+            {
+                var user = _userLogic.Add(_migration.GiveMeUser());
+                user.Id = _userLogic.GetUserId(user.Username);
+                FolderRoot = user.RootFolder;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("The format is not correct: " + e.Message);
+            }
+        }
+    }
+}
