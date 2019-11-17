3c7d7bd76e3e3846b2271bbed4a3306e6b177a09
Juanpa <sobraljuanpa@gmail.com>
2019-11-17 11:11:33 -0300

    Add folder component ready

--- twodrive-frontend/src/app/components/folder-add-form/folder-add-form.component.ts
+++ twodrive-frontend/src/app/components/folder-add-form/folder-add-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Folder } from "src/app/models/folder";
+import { Observable } from 'rxjs';
+import { FolderService } from 'src/app/services/folder.service';
+
 @Component({
   selector: 'app-folder-add-form',
   templateUrl: './folder-add-form.component.html',
   styleUrls: ['./folder-add-form.component.css']
 })
 export class FolderAddFormComponent implements OnInit {
+  folderName: string;
+  selectedFolder: Folder;
+  userFolders: Observable<Folder[]>;
 
-  constructor() { }
+  constructor(
+    private folderService: FolderService
+  ) { }
 
   ngOnInit() {
+    this.userFolders = this.folderService.getOwnedFolders();
+  }
+
+  addFolder() {
+    console.log(this.folderName);
+    console.log(this.selectedFolder);
   }
 
 }
